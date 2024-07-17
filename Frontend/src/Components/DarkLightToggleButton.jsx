cefaac03ce91c7c35a572193eed0f665a2c1c54d
vishalkushwaha2003 <vishal2003kushwaha@gmail.com>
2024-07-18 00:56:34 +0530

    dynamically change the colo of toggle itself

--- Frontend/src/Components/DarkLightToggleButton.jsx
+++ Frontend/src/Components/DarkLightToggleButton.jsx
@@ -5,6 +5,7 @@ import { useState } from 'react';
 function DarkLightToggleButton() {
   const [check,setCheck]=useState(false);
   const { theme, toggleTheme, color, changeColor } = useTheme();
+  console.log(color)
   const handleCheck = () => {
     setCheck(!check);
   
@@ -15,13 +16,15 @@ function DarkLightToggleButton() {
   return (
     <label className="switch" >
   <input className="checkbox" type="checkbox" onClick={handleCheck} />
-  <span className={`slider round   ${!check?'before:bg-slate-700 shadow-[inset_1px_1px_5px_0px_rgb(51,65,85)] ': 'before:bg-red-700'}`} style={{
+  <span 
+  className={`slider round ${!check ? 'before:bg-slate-700' : ''}`} 
+  style={{
+    boxShadow: `inset 1px 1px 5px 0 ${check ? color : 'rgb(51, 65, 85)'}`,
+    '--dynamic-before-color': check ? color : 'rgb(255, 0, 0)' ,
+  }}
+></span>
 
-    // boxShadow: '1px 1px 5px 0 rgb(51 65 85) inset'
-    // '&:before':{boxShadow: '1px 1px 5px 0 rgb(51 65 85) inset'},
-    
-    
-  }}></span>
+
 
 </label>
   )
