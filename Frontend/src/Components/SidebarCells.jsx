0ffc66bf4821e6686102fd6b78f2f5d8ac02be8b
vishalkushwaha2003 <vishal2003kushwaha@gmail.com>
2024-07-19 15:11:45 +0530

    variably adjust the maincontent width according to sidebar

--- Frontend/src/Components/SidebarCells.jsx
+++ Frontend/src/Components/SidebarCells.jsx
@@ -4,7 +4,7 @@ import DvrOutlinedIcon from '@mui/icons-material/DvrOutlined';
 function SidebarCells({ name, isOpen }) {
   console.log(isOpen);
   return (
-    <li className={`p-1 dark:hover:bg-slate-700 border-slate-500 hover:bg-slate-300 hover:cursor-pointer text-slate-950 dark:text-slate-300 lg:text-xl md:text-lg sm:text-sm text-xs flex gap-1 ${isOpen ? 'justify-start' : 'justify-end'} items-center text-nowrap overflow-x-scroll`}>
+    <li className={`p-1 dark:hover:bg-slate-700 border-slate-500 hover:bg-slate-300 hover:cursor-pointer text-slate-950 dark:text-slate-300 lg:text-xl md:text-lg sm:text-sm text-xs flex gap-1 ${isOpen ? 'justify-start' : 'justify-end'} px-2 sm:px-3 md:px-4 items-center text-nowrap overflow-x-scroll`}>
       <DvrOutlinedIcon />
       {isOpen && <div>{name}</div>}
     </li>
