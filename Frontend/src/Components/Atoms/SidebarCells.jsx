14ab90351f8a6cb20f914b2432ff67ae8b1b9cd5
vishal kushwaha <vishal2003kushwaha@gmail.com>
2024-08-16 22:36:34 +0530

    make responsive for all devices

--- Frontend/src/Components/Atoms/SidebarCells.jsx
+++ Frontend/src/Components/Atoms/SidebarCells.jsx
@@ -9,7 +9,7 @@ function SidebarCells({ name, isOpen,icon,setClickCell }) {
   }
          
   return (
-    <li className={`p-1 dark:hover:bg-slate-700 border-slate-500 hover:bg-slate-300 hover:cursor-pointer text-slate-950 dark:text-slate-300  md:text-lg sm:text-sm text-xs flex gap-1 ${isOpen ? 'justify-start' : 'justify-end'} px-2 sm:px-3 md:px-4 items-center text-nowrap overflow-x-scroll`} onClick={handleClick}>
+    <li className={`p-1 dark:hover:bg-slate-700 border-slate-500 hover:bg-slate-300 hover:cursor-pointer text-slate-950 dark:text-slate-300  md:text-lg sm:text-sm text-xs flex gap-1  px-2 sm:px-3 md:px-4 items-center text-nowrap overflow-x-scroll transition-all duration-200 transform`} onClick={handleClick}>
       <div style={{color:color}}>
       {icon}
       </div>
--- Frontend/src/Components/mainContent/SchoolDashboard/SchoolDashboardCard.jsx
+++ Frontend/src/Components/mainContent/SchoolDashboard/SchoolDashboardCard.jsx
@@ -8,7 +8,7 @@ function SchoolDashboardCard({ grade }) {
   const sections = ["A", "B", "C", "D"];
 
   return (
-    <div className="h-full w-full sm:border-2 border-[0.1px] border-slate-500 shadow-md hover:shadow-slate-400 dark:hover:shadow-slate-600 duration-200 rounded-md bg-slate-100 dark:bg-slate-700 text-slate-900 dark:text-slate-200 hover:cursor-pointer">
+    <div className="h-40 w-full sm:border-2 border-[0.1px] border-slate-500 shadow-md hover:shadow-slate-400 dark:hover:shadow-slate-600 duration-200 rounded-md bg-slate-100 dark:bg-slate-700 text-slate-900 dark:text-slate-200 hover:cursor-pointer">
       <div
         className=" h-[50%] lg:text-6xl font-bold md:text-5xl sm:text-4xl text-2xl flex justify-center items-center"
         style={{ color: color }}
@@ -16,15 +16,15 @@ function SchoolDashboardCard({ grade }) {
         {grade}
       </div>
 
-      <div className="flex h-[20%] justify-evenly items-center lg:text-md lg:font-medium md:text-sm md:font-normal sm:text-xs sm:font-normal text-[10px] font-thin">
+      <div className="flex h-[20%] justify-evenly items-center lg:text-md  md:text-sm  text-xs font-medium  ">
         <div>Boys : {totalBoys}</div>
         <div>Girls : {totalGirls}</div>
       </div>
-      <div className="h-[30%] flex justify-around items-center lg:text-xl md:font-medium md:text-lg sm:text-lg sm:font-medium text-xs font-medium ">
+      <div className="h-[30%] flex justify-around items-center lg:text-xl  md:text-lg text-md   font-medium ">
         {sections.map((section, index) => (
           <div
             key={index}
-            className=" sm:px-2 my-2 px-1  rounded-sm sm:rounded-md bg-slate-100 dark:bg-slate-700 shadow-md shadow-slate-400 dark:shadow-slate-500"
+            className=" px-2 my-2   rounded-sm sm:rounded-md bg-slate-100 dark:bg-slate-700 shadow-md shadow-slate-400 dark:shadow-slate-500"
           >
             {section}
           </div>
